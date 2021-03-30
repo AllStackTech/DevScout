a9b5cc32df90c417722a0d0580086203aab5a63f
tejastn10 <tejastn10@gmail.com>
2021-03-31 00:00:27 +0530

    🧱 UPDATE: User Entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,24 +1,29 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
-@Entity('users')
+@Entity("users")
 export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-    @PrimaryGeneratedColumn()
-    id!: number;
+  @Column({ unique: true })
+  userName: string;
 
-    @Column({unique: true})
-    userName: string;
+  // @Column({unique: true})
+  // email!: string;
 
-    // @Column({unique: true})
-    // email!: string;
+  // @Column()
+  // password!: string;
 
-    // @Column()
-    // password!: string;
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @CreateDateColumn()
-    createdAt: Date;
-
-    @UpdateDateColumn()
-    updatedAt: Date;
-
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
